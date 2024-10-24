646313720307e6e291629da96225e1d3421a65c7
weilinfox <caiweilin@iscas.ac.cn>
2024-10-24 16:37:54 +0800

    Add admin format-manifest test
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- testcases/ruyi-basic/ruyi-admin.bash
+++ testcases/ruyi-basic/ruyi-admin.bash
@@ -3,6 +3,9 @@
 
 export RUYI_DEBUG=x
 
+tmp_path=/tmp/rit-ruyi-basic-ruyi-admin
+mkdir -p "$tmp_path"
+
 ruyi admin manifest $0
 # CHECK-SAME: [
 # CHECK: "name":
@@ -23,3 +26,15 @@ ruyi admin manifest --format toml $0
 # CHECK-NEXT: sha256 =
 # CHECK-NEXT: sha512 =
 
+ruyi admin manifest --format toml $0 > "$tmp_path"/test.toml
+ruyi admin format-manifest "$tmp_path"/test.toml
+cat "$tmp_path"/test.toml
+# CHECK-LABEL: format = "v1"
+# CHECK-EMPTY:
+# CHECK-SAME: [metadata]
+# CHECK: desc =
+# CHECK: vendor =
+# CHECK: [[distfiles]]
+# CHECK: [distfiles.checksums]
+
+rm -rf "$tmp_path"
