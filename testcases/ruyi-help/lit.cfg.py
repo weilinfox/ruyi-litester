9902939ed2b0afb88e87cf7b1350b685dac931ff
weilinfox <caiweilin@iscas.ac.cn>
2024-10-29 18:01:54 +0800

    Inherit PATH and PYTHONPATH env before running ruyi-help testsuite
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- testcases/ruyi-help/lit.cfg.py
+++ testcases/ruyi-help/lit.cfg.py
@@ -3,3 +3,8 @@ import lit.formats
 config.name = "RuyiHelp"
 config.test_format = lit.formats.ShTest(True)
 config.suffixes = ['.bash']
+
+if os.environ.get('PATH'):
+    config.environment['PATH'] = os.environ.get('PATH')
+
+config.environment['PYTHONPATH'] = os.environ.get('PYTHONPATH', '')
