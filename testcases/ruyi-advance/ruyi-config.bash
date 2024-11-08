b681eeccd85720c0a094c42a711f4589928156af
weilinfox <caiweilin@iscas.ac.cn>
2024-11-08 15:51:05 +0800

    ruyi-config: backup old config file and create test config file
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- testcases/ruyi-advance/ruyi-config.bash
+++ testcases/ruyi-advance/ruyi-config.bash
@@ -6,12 +6,26 @@ export RUYI_DEBUG=x
 tmp_path="/tmp/rit-ruyi-advance-ruyi-config"
 tmp_repo="$tmp_path"/packages-index
 
-cfg_f="~/.config/ruyi/config.toml"
+cfg_d="~/.config/ruyi"
+cfg_f="$cfg_d/config.toml"
 cfg_bak="$tmp_path"/config.toml
 
 rm -rf "$tmp_path"
-mkdir -p "$tmp_path"
+mkdir -p "$cfg_d" "$tmp_path"
 
+# NOTE: backup old config file
+[ -f "$cfg_f" ] && mv -v "$cfg_f" "$cfg_bak"
+
+# NOTE: create test config
+function config_create() {
+	cat >"$cfg_f" <<EOF
+[repo]
+remote = "https://github.com/ruyisdk/packages-index.git"
+branch = "main"
+EOF
+}
+
+config_create
 cat >>"$cfg_f" <<EOF
 local = "$tmp_path"
 EOF
@@ -23,20 +37,20 @@ echo Check reconfigured local repo
 # CHECK-NEXT: 0
 rm -rf "$tmp_repo"
 
-cp "$cfg_bak" "$cfg_f"
+config_create
 echo Check bad remote repo
 # CHECK-LABEL: Check bad remote repo
 sed -i "s|remote.*|remote = \"https://wrong_magic\"|" $cfg_f
 ruyi update
 # CHECK: wrong_magic
 
-cp "$cfg_bak" "$cfg_f"
+config_create
 echo Check bad branch
 # CHECK-LABEL: Check bad branch
 sed -i "s|branch.*|branch = \"wrong_magic\"|" $cfg_f
 ruyi update
 # CHECK: wrong_magic
 
-mv "$cfg_bak" "$cfg_f"
+[ -f "$cfg_bak" ] && mv -v "$cfg_bak" "$cfg_f"
 rm -rf "$tmp_repo"
 
