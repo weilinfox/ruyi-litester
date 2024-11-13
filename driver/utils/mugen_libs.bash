485bf9beebebdf836d3ac903b49b104f7e7f9bd1
weilinfox <caiweilin@iscas.ac.cn>
2024-11-13 18:26:33 +0800

    Mugen CHECK_RESULT function
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- driver/utils/mugen_libs.bash
+++ driver/utils/mugen_libs.bash
@@ -18,6 +18,29 @@ function LOG_ERROR() {
 	LOG_PRINT "ERROR" $@
 }
 
+function CHECK_RESULT() {
+	local result=$1
+	local result_expect=${2-0}
+	local check_mode=${3-0}
+	local error_msg=$4
+	local exit_on_error=${5-0}
+
+	if [ -z "$result" ] || [ -z "$result_expect" ] || [ -z "$check_mode" ]; then
+		LOG_ERROR "Too few argument!"
+	fi
+
+	if [[ "$result" != "$result_expect" ]]; then
+		LOG_ERROR "$error_msg"
+		LOG_ERROR "${BASH_SOURCE[1]} line ${BASH_LINENO[0]}"
+		((RIT_MUGEN_ERROR_COUNT++))
+		if [ $exit_on_error -eq 1 ]; then
+			exit 1
+		fi
+	fi
+
+	return 0
+}
+
 function PKG_INSTALL() {
 }
 
