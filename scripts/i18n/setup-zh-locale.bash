5b1206ad8cca305e31360bfdd60472cf0a689b95
Cyl18 <cyl18a@gmail.com>
2024-12-13 00:56:04 -0800

    Add Fedora i18n support

--- scripts/i18n/setup-zh-locale.bash
+++ scripts/i18n/setup-zh-locale.bash
@@ -1,7 +1,20 @@
 #!/bin/bash
-echo zh_CN.UTF-8 UTF-8 | sudo tee /etc/locale.gen
 
-sudo locale-gen
+for i in $HOST_FEATURES; do
+if [[ "$i" == "ubuntu" ]] || [[ "$i" == "debian" ]]; then
+    echo zh_CN.UTF-8 UTF-8 | sudo tee /etc/locale.gen
+
+    sudo locale-gen
+	break
+elif [[ "$i" == "gentoo" ]]; then
+
+	break
+elif [[ "$i" == "fedora" ]]; then
+    sudo localedef -c -i zh_CN -f UTF-8 zh_CN.UTF-8
+
+	break
+fi
+done
 
 cat > /tmp/locale.sh <<EOF
 export LANG=zh_CN.UTF-8
