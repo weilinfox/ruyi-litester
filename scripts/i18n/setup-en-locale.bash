c1e9f35b6c646c5cdad543a1686b3e4f52a27b10
Cyl18 <cyl18a@gmail.com>
2024-12-14 07:40:54 -0800

    Add gentoo support & fix some error

--- scripts/i18n/setup-en-locale.bash
+++ scripts/i18n/setup-en-locale.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./setup-locale.bash en_US
+scripts/i18n/setup-locale.bash en_US
--- scripts/i18n/setup-locale.bash
+++ scripts/i18n/setup-locale.bash
@@ -1,13 +1,11 @@
 #!/bin/bash
 for i in $RIT_CASE_FEATURES; do
-if [[ "$i" == "ubuntu" ]] || [[ "$i" == "debian" ]]; then
+if [[ "$i" == "ubuntu" ]] || [[ "$i" == "debian" ]] || [[ "$i" == "gentoo" ]]; then
     echo $1.UTF-8 UTF-8 | sudo tee /etc/locale.gen
 
     sudo locale-gen
     break
-elif [[ "$i" == "gentoo" ]]; then
 
-    break
 elif [[ "$i" == "fedora" ]]; then
     sudo localedef -c -i $1 -f UTF-8 $1.UTF-8
 
