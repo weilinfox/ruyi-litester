15e7922aeee680061edc1f326afa3c82a62aab6b
Cyl18 <cyl18a@gmail.com>
2024-12-13 20:38:42 -0800

    Simplify i18n script

--- scripts/i18n/setup-en-locale.bash
+++ scripts/i18n/setup-en-locale.bash
@@ -1,16 +1,3 @@
 #!/bin/bash
-for i in $HOST_FEATURES; do
-if [[ "$i" == "ubuntu" ]] || [[ "$i" == "debian" ]]; then
-    echo en_US.UTF-8 UTF-8 | sudo tee /etc/locale.gen
 
-    sudo locale-gen
-	break
-elif [[ "$i" == "gentoo" ]]; then
-
-	break
-elif [[ "$i" == "fedora" ]]; then
-    localedef -c -i en_US -f UTF-8 en_US.UTF-8
-
-	break
-fi
-done
+./setup-locale.bash en_US
