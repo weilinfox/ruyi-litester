8b18a25a3a516325239655a4be699af49510889b
Cyl18 <cyl18a@gmail.com>
2024-12-16 00:44:15 -0800

    Fix ALLOW_RETRIES

--- testcases/ruyi-basic/ruyi-install.bash
+++ testcases/ruyi-basic/ruyi-install.bash
@@ -1,7 +1,7 @@
 # NOTE: Test ruyi install and extract
 # NOTE: ``ruyi install`` outputs are printed to stderr
 # RUN: bash %s 2>&1 | FileCheck %s
-# ALLOW_RETRIES: 1
+# ALLOW_RETRIES: 0
 
 # export RUYI_DEBUG=x
 
