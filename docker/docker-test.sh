ba9dc15f2d8bb19ea55e52cf030121ee13feeb97
Cyl18 <cyl18a@gmail.com>
2024-12-15 23:02:54 -0800

    Fix docker test logs not being copied completely

--- docker/docker-test.sh
+++ docker/docker-test.sh
@@ -41,11 +41,14 @@ fi
 docker rm ruyi-test-docker || true
 
 docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t $docker_tag -f docker/distros/$dockerfile . && \
-docker run --name ruyi-test-docker -it $docker_tag "$@"   
+docker run -e DOCKER=true --name ruyi-test-docker -it $docker_tag "$@"   
 #docker run -e RUYI_TELEMETRY_OPTOUT=1 --name ruyi-test-docker -it $docker_tag "$@"
 
 # 复制 log 并删除 container 和 image
-docker cp ruyi-test-docker:/ruyi-litester/mugen_log/ . && docker rm ruyi-test-docker
+docker cp ruyi-test-docker:/ruyi-litester/mugen_log/ .
+rm -rf logs
+docker cp ruyi-test-docker:/ruyi-litester/logs/ ./ 
+docker rm ruyi-test-docker
 docker rmi $docker_tag
 
 
